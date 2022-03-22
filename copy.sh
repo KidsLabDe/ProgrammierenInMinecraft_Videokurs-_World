3f2b0bb34715704d0273c25d1e0e96f332b62f67
KingBBQ <gw@communitylabs.de>
2022-03-22 15:59:13 +0100

    - 11. stunde - redstone!

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -r /opt/mscs/worlds/StreamingKurs/streamingkurs/
-cp -r streamingkurs /opt/mscs/world?/StreamingKurs/
+cp -r streamingkurs /opt/mscs/worlds/StreamingKurs/
 chown -R minecraft:minecraft /opt/mscs/worlds/*
